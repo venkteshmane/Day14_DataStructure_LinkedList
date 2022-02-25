2b5816e4c9cb4f8a87a4260ae3b7fc34b8896ee2
venktesh.mane18@gmail.com <venktesh.mane18@gmail.com>
2022-02-25 19:04:18 +0530

    UC4_Insert_Element_Between_Node

--- LinkedList.java
+++ LinkedList.java
@@ -1,7 +1,6 @@
 package Linkedlist;
 import java.util.Scanner;
 
- //created linked list to push the data inside the list.
 class Node {
 	int data;
 	Node next;
@@ -10,25 +9,41 @@ class Node {
 		this.data = data;
 		this.next = null;
 	}
+
+	@Override
+	public String toString() {
+		return "Node{" + "data=" + data + ", next=" + next;
+	}
 }
+
 class operations {
 	Node head;
 	Node tail;
 
-	public void pushData(int data) {
+	public Node pushData(int data) {
+		// created the object of the Node class to get the address of the Node and
+		// passing the "data" it will store in constructor
 		Node newnode = new Node(data);
 		if (head == null) {
 			head = newnode;
+			tail = newnode;
 		} else {
+			/*
+			 * swapping the value to get the new node address
+			 */
 			Node temp = head;
 			this.head = newnode;
 			newnode.next = temp;
 		}
+		return newnode;
 	}
+
 	public void print() {
 		if (head == null)
 			System.out.println("Empty LinkList");
+
 		else {
+
 			Node temp = head;
 			while (temp != null) {
 				System.out.print(temp.data + " -> ");
@@ -36,8 +51,7 @@ class operations {
 			}
 		}
 	}
-	 //Used the function to add the element at the end. and applied switch case to
-	 //select where to add the element.
+
 	public void append(int data) {
 		Node newnode = new Node(data);
 		if (head == null) {
@@ -48,42 +62,65 @@ class operations {
 			tail = newnode;
 		}
 	}
+
+	public void inBetweenInsert(Node previousNode, Node newNode) {
+		Node tempNode = previousNode.next;
+		previousNode.next = newNode;
+		newNode.next = tempNode;
+	}
 }
 
-public class LinkedList {
+class LinkList {
+	static operations link = new operations();
 
 	public static void addDataStart() {
-		operations link = new operations();
 		link.pushData(70);
 		link.pushData(30);
 		link.pushData(56);
 		link.print();
 	}
 
-	public static void addDataEnd() {
-		operations link = new operations();
+	public void addDataEnd() {
 		link.append(56);
 		link.append(30);
 		link.append(70);
 		link.print();
 	}
 
-	public void main(String[] args) {
+	public void insertInBitween() {
+		Node secondnode = link.pushData(70);
+		Node firstnode = link.pushData(56);
+		Node newnode = new Node(30);
+		System.out.println("Before:");
+		link.print();
+		link.inBetweenInsert(firstnode, newnode);
+		System.out.println("\nAfter:");
+		link.print();
+	}
+}
+
+public class UC4_toInsertElementBetweenTheNodes {
+	public static void main(String[] args) {
+		LinkList obj = new LinkList();
 		System.out.println("*** Welcome To LinkList Program ***");
 		Scanner sc = new Scanner(System.in);
 		System.out.println();
 		System.out.println("Press 1 to add data at start");
 		System.out.println("Press 2 to add data at end");
+		System.out.println("Press 3 to insert data in between");
 		int input = sc.nextInt();
 		switch (input) {
 		case 1:
-			addDataStart();
+			obj.addDataStart();
 			break;
 		case 2:
-			addDataEnd();
+			obj.addDataEnd();
+			break;
+		case 3:
+			obj.insertInBitween();
 			break;
 		default:
-			System.out.println("Invalid Choice");
+			System.out.println("Invalid Choice");	
 		}
 	}
 }
