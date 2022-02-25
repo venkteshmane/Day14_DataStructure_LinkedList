4a1a9fc342186970617b3133ccf877ed393ac230
venktesh.mane18@gmail.com <venktesh.mane18@gmail.com>
2022-02-25 18:27:55 +0530

    UC2_CreateLinkedList_toAddElement

--- LinkedList.java
+++ LinkedList.java
@@ -1,32 +1,50 @@
 package Linkedlist;
+
 class Node {
-	
 	int data;
 	Node next;
-	
-	public Node(int data) {
+public Node(int data) {
 		this.data = data;
 		this.next = null;
+	}	
+}
+
+class operations {
+	// Taking variables to store the head.
+	Node head;
+	public void pushData(int data) {
+		
+		//created the object of the Node class to get the address of the Node and
+		// passing the "data" it will store in constructor
+		Node newnode = new Node(data);
+		if (head == null) {
+			head = newnode;
+		} else {
+			Node temp = head;
+			this.head = newnode;
+			newnode.next = temp;			
+		}
+	}
+	public void print() {
+		if (head == null)
+			System.out.println("Empty LinkedList");
+		else {
+			Node temp = head;
+			while (temp != null) {
+				System.out.print(temp.data + " -> ");
+				temp = temp.next;
+			}
+		}
 	}
 }
 public class LinkedList {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To LinkedList Program");
-		Node firstnode = new Node(56);
-		Node secondnode = new Node(30);
-		Node thirdnode = new Node(70);
-		// created a head variable to store the first node address then we are storing
-		// the address of next nodes in variable next
-		Node head = firstnode;
-		firstnode.next = secondnode;
-		secondnode.next = thirdnode;
-		Node temp = head;
-		// storing the head address into temp variable for print the values.
-		while (temp != null) {
-			System.out.println(temp.data+" ");
-			temp = temp.next;
+		System.out.println("Welcome To LinkList Program");
+		operations link = new operations();
+		link.pushData(70);
+		link.pushData(30);
+		link.pushData(56);
+		link.print();
 		}
-	}
 }
-
